37d5d82df0b32f2e8c956ac4979f95af8d2dda43
Ashvin <ashvinsunga@yahoo.com.ph>
2022-05-23 23:08:12 +0800

    fetch api data function defined

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,19 @@
 import React, { useEffect, useState } from 'react';
 
 function Home() {
+  const [countries, setCountries] = useState([]);
+  const [mode, setMode] = useState([true]);
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
+  const getCountries = async () => {
+    const res = await fetch('https://restcountries.com/v3.1/all');
+    const data = await res.json();
+    await setCountries(data);
+  };
+
   return (
     <div>
       <h2> This is Home</h2>
