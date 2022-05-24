daa9846d9ae85253d0a828b290e83722800ad082
Ashvin <ashvinsunga@yahoo.com.ph>
2022-05-24 17:11:38 +0800

    search box ui done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,10 @@ import React, { useEffect, useState } from 'react';
 
 function Home() {
   const [countries, setCountries] = useState([]);
-  const [mode, setMode] = useState([true]);
 
-  useEffect(() => {
-    getCountries();
-  }, []);
+  // useEffect(() => {
+  //   getCountries();
+  // }, []);
 
   const getCountries = async () => {
     const res = await fetch('https://restcountries.com/v3.1/all');
@@ -15,8 +14,16 @@ function Home() {
   };
 
   return (
-    <div>
-      <h2> This is Home</h2>
+    <div className="flex justify-center">
+      <i className="fa fa-search my-auto -mr-8 z-10 pl-3 py-5 rounded-md text-gray-400"></i>
+      <input
+        type="text"
+        placeholder="Filter Countries by Name"
+        // onChange={(countryName) =>
+        //   searchCountryByName(countryName.target.value)
+        // }
+        className="pl-10 p-2 shadow-md  rounded-md w-9/12"
+      />
     </div>
   );
 }
