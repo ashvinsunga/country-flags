a43eab021c1452ddf15c95c1e2516842e5c21f1a
Ashvin <ashvinsunga@yahoo.com.ph>
2022-05-24 21:11:05 +0800

    Thumbinfo component defined

---
+++ src/components/ThumbInfo.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+export default function ThumbInfo({
+  countryFlag,
+  countryCode,
+  countryName,
+  countryTimezones,
+  countryCallingCode,
+}) {
+  return (
+    <div className="grid grid-flow-col content-center rounded-lg shadow-lg bg-white p-2 border-4">
+      <div className="p-4">
+        <p className="text-xs">
+          <span className="text-gray-700">{countryCode}</span>
+        </p>
+        <p className="text-xs">
+          <span className="text-gray-700">{countryName}</span>
+        </p>
+        <p className="text-xs">
+          {countryTimezones.map((timezone) => (
+            <span>
+              {' '}
+              {countryTimezones.length > 1 ? timezone + ',' : timezone}
+            </span>
+          ))}
+        </p>
+        <p className="text-xs">
+          <span className="text-gray-700">{countryCallingCode}</span>
+        </p>
+      </div>
+      <div className="grid content-center justify-center">
+        <img
+          src={countryFlag}
+          className="h-32 max-h-20 max-w-3xl shadow-md border-2"
+          alt={countryName}
+        />
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,49 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import ThumbInfo from '../components/ThumbInfo';
 
 function Home() {
   const [countries, setCountries] = useState([]);
 
-  // useEffect(() => {
-  //   getCountries();
-  // }, []);
+  useEffect(() => {
+    getCountries();
+  }, []);
 
   const getCountries = async () => {
     const res = await fetch('https://restcountries.com/v3.1/all');
     const data = await res.json();
+    // console.log(data);
+
     await setCountries(data);
   };
 
   return (
-    <div className="flex justify-center">
-      <i className="fa fa-search my-auto -mr-8 z-10 pl-3 py-5 rounded-md text-gray-400"></i>
-      <input
-        type="text"
-        placeholder="Filter Countries by Name"
-        // onChange={(countryName) =>
-        //   searchCountryByName(countryName.target.value)
-        // }
-        className="pl-10 p-2 shadow-md  rounded-md w-9/12"
-      />
+    <div>
+      <div className="grid grid-cols-4 justify-center">
+        <i className="place-self-end fa fa-search my-auto -mr-8 z-10 pl-3 py-5 rounded-md text-gray-400"></i>
+        <input
+          type="text"
+          placeholder="Filter Countries by Name"
+          // onChange={(countryName) =>
+          //   searchCountryByName(countryName.target.value)
+          // }
+          className="pl-10 p-2 shadow-md rounded-3xl w-9/12  m-2 border-slate-600 col-span-3"
+        />
+      </div>
+
+      <div className=" grid grid-cols-4 gap-2">
+        {countries.map((country, index) => (
+          <Link to={{ pathname: 'details', state: country }} key={index}>
+            <ThumbInfo
+              countryFlag={country.flags.png}
+              countryCode={country.cca2}
+              countryName={country.name.common}
+              countryTimezones={country.timezones}
+              countryCallingCode={country.ccn3}
+            />
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
