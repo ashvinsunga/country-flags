fa3905d3e6a31ec455c1708a3a71ed6cbcd33934
Ashvin <ashvinsunga@yahoo.com.ph>
2022-06-18 10:35:31 +0800

    done

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -14,7 +14,7 @@ function Details() {
       <div className="border-4 border-solid rounded-lg border-neutral-400 px-5 pb-8 w-1/4 h-fit sm:w-4/6 md:w-3/6 lg:w-2/3">
         <div className="grid grid-col-3 mt-5">
           <img
-            className="place-self-center lg:h-80"
+            className="place-self-center lg:h-70"
             src={state.flags.png}
             alt={state.name.common}
           />
@@ -61,12 +61,6 @@ function Details() {
       </div>
       {/* Buttons */}
       <div className="grid grid-rows-2 mt-2 md:text-xl">
-        <div className="my-2">
-          <button className="px-6 py-2 bg-white text-stone-900 shadow-md w-44 shadow-stone-400 md:w-80">
-            Show on the map
-          </button>
-        </div>
-
         <div>
           <button
             className="px-10 py-2 bg-white text-stone-900 shadow-md w-44 shadow-stone-400 md:w-80"
